621863b8fd4b65053f8fc7a5cb4d057f62b006a8
Ajay Verma <aryanav30@gmail.com>
2026-09-23 20:13:02 +0530

    Removed motion import

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
-import { motion } from 'framer-motion';
 
 function Footer() {
   const currentYear = new Date().getFullYear();
